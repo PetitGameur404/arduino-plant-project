60ff7883e6de76c667cd42147e8463ab7589068c
PetitGameur404 <petitgameurytprofessionnel@gmail.com>
2026-09-19 11:18:37 +0200

    Fix loop function and update pump control logic

--- test-water-pompe.ino
+++ test-water-pompe.ino
@@ -3,10 +3,14 @@ const int PomPin = 7;
 void setup() {
   Serial.begin(9600);
   pinMode(PomPin, OUTPUT);
-}
 
-void loop() {
   digitalWrite(PomPin, LOW);
   Serial.println("Pompe on!");
   delay(1000);
+
+  digitalWrite(PomPin, HIGH);
+  Serial.println("Pompe off!");
+}
+
+void loop() {
 }
